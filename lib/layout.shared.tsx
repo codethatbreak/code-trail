3483b94a8b4e27e2643f0be8513bc219018ca6b6
Aakash Kumar <aakash.kumar0407@gmail.com>
2026-05-25 20:59:48 +0530

    chore: initial setup

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -1,11 +1,18 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
-import { appName, gitConfig } from './shared';
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import { appName, gitConfig } from "./shared";
+
+import { Waypoints } from "lucide-react";
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
       // JSX supported
-      title: appName,
+      title: (
+        <>
+          <Waypoints size={20} />
+          <span className="font-medium in-[.uwu]:hidden">{appName}</span>
+        </>
+      ),
     },
     githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
   };
