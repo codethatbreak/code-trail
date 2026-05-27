a6d54a3512a09109b852ba6e8e996e0dfbd1e300
Aakash Kumar <aakash.kumar0407@gmail.com>
2026-05-27 10:04:51 +0530

    chore: Add notes for graph

--- source.config.ts
+++ source.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig, defineDocs } from 'fumadocs-mdx/config';
-import { metaSchema, pageSchema } from 'fumadocs-core/source/schema';
+import { defineConfig, defineDocs } from "fumadocs-mdx/config";
+import { metaSchema, pageSchema } from "fumadocs-core/source/schema";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 
 // You can customize Zod schemas for frontmatter and `meta.json` here
 // see https://fumadocs.dev/docs/mdx/collections
 export const docs = defineDocs({
-  dir: 'content/docs',
+  dir: "content/docs",
   docs: {
     schema: pageSchema,
     postprocess: {
@@ -19,5 +21,8 @@ export const docs = defineDocs({
 export default defineConfig({
   mdxOptions: {
     // MDX options
+    remarkPlugins: [remarkMath],
+    // Place it at first, it should be executed before the syntax highlighter
+    rehypePlugins: (v) => [rehypeKatex, ...v],
   },
 });
